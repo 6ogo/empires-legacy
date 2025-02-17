57207291edc8cc1bc2fc239d0877cf66ee83a767
George <106427405+6ogo@users.noreply.github.com>
2025-02-17 17:23:45 +0000

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,10 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './types';
 
-//const supabaseUrl = "https://vrkxovunejlbqphxqnuy.supabase.co";
-//const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZya3hvdnVuZWpsYnFwaHhxbnV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NTAxNzQsImV4cCI6MjA1NTAyNjE3NH0.lZxebcdjnUKDy2RG16Qd0aZgr7tQO9ONaQzQ_cVuPeE";
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.supabaseUrl;
+const supabaseAnonKey = import.meta.env.supabaseAnonKey;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
